27f1d1adcf452f5f962554654084fba0cfc847a8
sweerasingha <sachiprojects@gmail.com>
2024-03-11 09:27:26 +0530

    Add Item Function Created Successfully

--- src/Pages/AddItem.js
+++ src/Pages/AddItem.js
@@ -5,21 +5,62 @@ import { AiFillHome, AiFillSetting } from "react-icons/ai";
 import { RiAdminFill, RiDashboardFill, RiLoginBoxFill, RiNotificationFill } from "react-icons/ri";
 import { IoPeople } from "react-icons/io5";
 import { Link } from 'react-router-dom';
+import axios from 'axios';
 
 
 const Navbar = () => {
   const [open, setOpen] = useState(true);
   const [submenuOpen, setSubmenuOpen] = useState(false);
-  const [imagePreview, setImagePreview] = useState(null);
+  const [productDetails, setProductDetails] = useState({
+    name: '',
+    description: '',
+    category: '',
+    foodType: '',
+    readyTime: '',
+    price: '',
+    rating: '',
+    images: []
+  });
+  const [imagePreview, setImagePreview] = useState([]);
+
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setProductDetails({ ...productDetails, [name]: value });
+  };
 
   const handleImageChange = (e) => {
-    const file = e.target.files[0];
-    if (file && file.type.substr(0, 5) === "image") {
-      setImagePreview(URL.createObjectURL(file));
-    } else {
-      setImagePreview(null);
+    const files = Array.from(e.target.files);
+    const updatedPreviewUrls = files.map(file => URL.createObjectURL(file));
+    setImagePreview(updatedPreviewUrls);
+    setProductDetails({ ...productDetails, images: files });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault(); // Prevents the default form submit action
+
+    const formData = new FormData();
+    for (const key in productDetails) {
+      if (key === "images") {
+        productDetails[key].forEach((file) => {
+          formData.append('images', file);
+        });
+      } else {
+        formData.append(key, productDetails[key]);
+      }
+    }
+
+    try {
+      const response = await axios.post('http://localhost:8002/product/create', formData, {
+        headers: {
+          'Content-Type': 'multipart/form-data',
+        },
+      });
+      console.log(response.data); // Handle the response as needed
+    } catch (error) {
+      console.error(error); // Handle errors as needed
     }
   };
+
 
   const Menu = [
     {
@@ -139,126 +180,86 @@ const Navbar = () => {
           ))}
         </ul>
       </div>
-
-
-
-      <div className="p-9 w-full h-auto bg-gray-200">
+       <div className="p-9 w-full h-auto bg-gray-200">
         <h1 className="text-4xl font-semibold">Add Item</h1>
-        <div class="mb-8 py-10" >
-
-        <div className="absolute top-[SIZE OF ORGANIZE BOX] right-10 mt-4 mr-4 ">
-          <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="display-images">
-            Display Images
-          </label>
-          <input className="block w-full text-sm text-gray-500 file:mr-4 file:py-2 file:px-4 file:rounded-full file:border-0 file:text-sm file:font-semibold file:bg-violet-50 file:text-violet-700 hover:file:bg-violet-100" id="display-images" type="file" onChange={handleImageChange} />
-          {imagePreview && (
-            <div className="mt-4">
-              <img src={imagePreview} alt="Preview" className="w-48 h-auto rounded-lg shadow-lg"/>
+        <form className="mb-8 py-10" onSubmit={handleSubmit}>
+          <div className="mb-4">
+            <label htmlFor="images" className="block text-gray-700 text-sm font-bold mb-2">Images</label>
+            <input type="file" multiple onChange={handleImageChange} className="block w-full text-sm text-gray-500 file:mr-4 file:py-2 file:px-4 file:rounded-full file:border-0 file:text-sm file:font-semibold file:bg-violet-50 file:text-violet-700 hover:file:bg-violet-100"/>
+            <div className="flex space-x-4 mt-4">
+              {imagePreview.map((src, index) => (
+                <img key={index} src={src} alt="Preview" className="w-20 h-20 rounded-lg shadow-lg"/>
+              ))}
             </div>
-          )}
-        </div>
-
-
-          <label class={`block ${open ? "w-72" : "w-20"} text-gray-700 mb-3 text-sm font-bold mb-5" for="item-title`}>
-            Item Title
-          </label>
-          <input class="shadow appearance-none border rounded w-80 py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="item-title" type="text" placeholder="Item Title" />
-          
-          
-          <label class="block  text-gray-700 text-sm font-bold mb-3 mt-10" for="py-10 item-description">
-            Item Description
-          </label>
-          <textarea class="shadow appearance-none border rounded w-1/2 py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="item-description" placeholder="Item Description"></textarea>
-
-          
-          
-          
-
-          <label class="block text-gray-700 text-sm font-bold mb-2 mt-10" for="price">
-            Price
-          </label>
-          <input class="shadow appearance-none border mt-3 rounded w-80 py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="price" type="text" placeholder="Price" />
-        </div>
-
-        <div class="relative bg-gray-300 rounded-lg shadow-lg p-6 w-full max-w-4xl">
-  <div class="flex flex-wrap -mx-3">
-    
-    <div class="w-full md:w-1/2 px-3 mb-6 md:mb-0">
-      <h2 class="text-lg font-semibold mb-4">Organize</h2>
-      <div class="mb-4">
-        <label class="block text-gray-700 text-sm font-bold mb-2" for="category">
-          Category
-        </label>
-        <select class="block appearance-none w-full bg-white border border-gray-200 text-gray-700 py-3 px-4 pr-8 rounded leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="category">
-          <option>Beverages</option>
-          <option>Snacks</option>
-          <option>Soft Drinks</option>
-          <option>Food</option>
-        </select>
-      </div>
-      <div class="mb-4">
-        <label class="block text-gray-700 text-sm font-bold mb-2" for="collection">
-          Collection
-        </label>
-        <input class="shadow appearance-none border rounded w-full py-3 px-4 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="collection" type="text" placeholder="Collection" />
-      </div>
-      <div class="mb-4">
-        <label class="block text-gray-700 text-sm font-bold mb-2" for="tags">
-          Tags
-        </label>
-        <input class="shadow appearance-none border rounded w-full py-3 px-4 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="tags" type="text" placeholder="Tags" />
-      </div>
-    </div>
-
-   
-    <div class="w-full md:w-1/2 px-10">
-      <h2 class="text-lg font-semibold mb-4">Variants</h2>
-      <div class="mb-4">
-        <label class="block text-gray-700 text-sm font-bold mb-2" for="option1">
-          Option 1
-        </label>
-        <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="option1" type="text" placeholder="Size" />
-      </div>
-      <div class="mb-4">
-        <label class="block text-gray-700 text-sm font-bold mb-2" for="option2">
-          Option 2
-        </label>
-        <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="option2" type="text" placeholder="Size" />
-      </div>
-      <button class="mt-4 bg-gray-500 hover:bg-black text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline">
-        Add another option
-      </button>
-    </div>
-  </div>
-</div>
-
-    
-
-        
-
-
-
-        <div class="flex justify-center w-1/2 items-center mt-10 space-x-10 p-4">
-
-          <button class="bg-gray-400 text-black-800 font-semibold py-2 px-4 w-40 rounded-lg shadow hover:bg-gray-300">
-            Discard
-          </button>
-
-
-          <button class="bg-gray-400 text-black-800 font-semibold py-2 px-4 w-40 rounded-lg shadow hover:bg-gray-300">
-            Save Draft
-          </button>
-
-
-          <button class="bg-gray-400 text-black-800 w-40 font-semibold py-2 px-4 rounded-lg shadow hover:bg-gray-300">
-            Publish Item
-          </button>
-        </div>
-
-
+          </div>
+          <div>
+            <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="name">
+              Name
+            </label>
+            <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="name" name="name" type="text" placeholder="Product Name" value={productDetails.name} onChange={handleInputChange} />
+          </div>
+          <div>
+            <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="description">
+              Description
+            </label>
+            <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="description" name="description" type="text" placeholder="Product Description" value={productDetails.description} onChange={handleInputChange} />
+          </div>
+          <div className="mb-4">
+            <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="category">
+              Category
+            </label>
+            <select 
+              id="category" 
+              name="category" 
+              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" 
+              value={productDetails.category} 
+              onChange={handleInputChange}
+            >
+              <option value="">Select a Category</option>
+              <option value="Beverages">Beverages</option>
+              <option value="Snacks">Snacks</option>
+              <option value="Soft Drinks">Soft Drinks</option>
+              <option value="Food">Food</option>
+            </select>
+          </div>
+          <div className="mb-4">
+            <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="foodType">
+              Food Type
+            </label>
+            <select 
+              id="foodType" 
+              name="foodType" 
+              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" 
+              value={productDetails.foodType} 
+              onChange={handleInputChange}
+            >
+              <option value="">Select Food Type</option>
+              <option value="Vegetarian">Vegetarian</option>
+              <option value="Non-Vegetarian">Non-Vegetarian</option>
+              <option value="Vegan">Vegan</option>
+            </select>
+          </div>
+          <div>
+            <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="readyTime">
+              Ready Time
+            </label>
+            <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="readyTime" name="readyTime" type="text" placeholder="Product Ready Time" value={productDetails.readyTime} onChange={handleInputChange} />
+          </div>
+          <div>
+            <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="price">
+              Price
+            </label>
+            <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="price" name="price" type="text" placeholder="Product Price" value={productDetails.price} onChange={handleInputChange} />
+          </div>
+          <div className="flex justify-center items-center mt-10 space-x-10 p-4">
+            <button type="submit" className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+              Submit
+            </button>
+          </div>
+        </form>
       </div>
     </div>
   );
-}
+};
 
 export default Navbar;
