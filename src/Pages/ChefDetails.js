e8391bbe0830b1625f6afbc19dee1e1618f6a4d2
SupuniJayasinghe <supuninavodya695@gmail.com>
2024-05-28 22:16:32 +0530

    GetChefDetails created.

--- src/Pages/ChefDetails.js
+++ src/Pages/ChefDetails.js
@@ -2,66 +2,103 @@ import React, { useContext } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { BsSearch } from 'react-icons/bs';
 import { ChefContext } from '../Components/ChefContext';
+import axios from 'axios';
+import { useEffect } from 'react';
+import { useState } from 'react';
 
 const ChefDetails = () => {
-  const { chefs } = useContext(ChefContext);
+  //const { chefs } = useContext(ChefContext);
   const navigate = useNavigate();
+  //const { setChefs } = useContext(ChefContext);
+  const [chefs, setChefs] = useState([]);
 
+
+  useEffect(() => {
+    getAllChefs();
+  }, []);
+  
+  const getAllChefs = () => {
+    const token = localStorage.getItem('token'); // get token from localStorage
+  
+    axios.get('http://34.224.26.99/admin/chefs', {
+      headers: {
+        'Authorization': `Bearer ${token}`
+      }
+    })
+    .then(response => {
+      console.log(response.data);
+  
+      if (Array.isArray(response.data)) {
+        setChefs(response.data);
+      } else {
+        setChefs([]);
+      }
+    })
+    .catch(error => {
+      console.error('There was an error!', error);
+    });
+  };
+
+  
   return (
     <div className="w-full h-auto bg-white p-9">
-      <div className="flex justify-between items-center">
-        <h1 className="text-4xl text-orange-800 font-semibold">Chef Details</h1>
+      <div className="flex items-center justify-between">
+        <h1 className="text-4xl font-semibold text-orange-800">Chef Details</h1>
         
         {/* Search Input - Adjust for mobile */}
         <div className="w-1/2 basis-full md:basis-auto md:flex md:items-center">
           <div className="relative w-full">
-            <BsSearch className="text-black text-lg absolute left-3 top-1/2 transform -translate-y-1/2 cursor-pointer" />
-            <input type="text" placeholder="Search " className="w-full pl-10 py-2 px-4 bg-orange-200 border rounded-lg" />
+            <BsSearch className="absolute text-lg text-black transform -translate-y-1/2 cursor-pointer left-3 top-1/2" />
+            <input type="text" placeholder="Search " className="w-full px-4 py-2 pl-10 bg-orange-200 border rounded-lg" />
           </div>
         </div>
         <button 
           onClick={() => navigate('/chef')} 
-          className="py-2 px-8 bg-orange-900 text-white rounded-md hover:bg-orange-600">
+          className="px-8 py-2 text-white bg-orange-900 rounded-md hover:bg-orange-600">
           Add Chef
         </button>
       </div>
       {chefs.length > 0 ? (
-        chefs.map((chef, index) => {
+        chefs.map((chef, _id) => {
           const imageUrl = chef.image ? URL.createObjectURL(chef.image) : null;
           return (
-            <div key={index} className="max-w-lg mx-auto p-8 bg-orange-100 shadow-md rounded-lg mt-10">
-              <h2 className="text-2xl font-bold mb-6 text-center">{chef.name}</h2>
+            <div key={_id} className="max-w-lg p-8 mx-auto mt-10 bg-orange-100 rounded-lg shadow-md">
+              <h2 className="mb-6 text-2xl font-bold text-center">{chef.name}</h2>
               {imageUrl && (
                 <div className="mb-4">
-                  <img src={imageUrl} alt="Chef Profile" className="w-full h-48 object-cover rounded-md" />
+                  <img src={imageUrl} alt="Chef Profile" className="object-cover w-full h-48 rounded-md" />
                 </div>
               )}
               <div className="mb-4">
                 <strong>Name:</strong> {chef.name}
               </div>
-              <div className="mb-4">
+              {/* <div className="mb-4">
                 <strong>Specialty:</strong> {chef.specialty}
-              </div>
-              <div className="mb-4">
+              </div> */}
+              {/* <div className="mb-4">
                 <strong>Years of Experience:</strong> {chef.experience}
+              </div> */}
+              <div className="mb-4">
+                <strong>Description:</strong> {chef.description}
               </div>
               <div className="mb-4">
-                <strong>Workplace:</strong> {chef.workplace}
+                <strong>Title:</strong> {chef.title}
               </div>
-              <div className="mb-4">
-                <strong>Address:</strong> {chef.address}
-              </div>
-              <div className="mb-4">
+              {/* <div className="mb-4">
                 <strong>Contact Number:</strong> {chef.contact}
-              </div>
+              </div> */}
               <div className="mb-4">
-                <strong>Email:</strong> {chef.email}
+                <img
+                src={`http://34.224.26.99/admin/chefs/${chef.photo}`}
+                className="object-cover object-center w-full h-full"
+                alt={chef.name}
+                />              
               </div>
             </div>
           );
         })
       ) : (
-        <div className="text-center mt-10 text-xl">No chef details available. Please add a chef first.</div>
+        <div className="mt-10 text-xl text-center">No chef details available. Please add a chef first.</div>
       )}
     </div>
   );
