d123c72ff2fc93508a3b2157cafc29a599999d60
SupuniJayasinghe <supuninavodya695@gmail.com>
2024-06-25 22:13:50 +0530

    header navigation is updated

--- src/Components/HeaderNavigation.js
+++ src/Components/HeaderNavigation.js
@@ -2,8 +2,39 @@ import React from 'react'
 import profilepic from '../Assets/Ellipse 6.png'
 import bell from '../Assets/Bell-off.svg'
 import menuicon from '../Assets/Chevron-down.png'
+import { useState, useEffect } from 'react';
+import axios from 'axios';
 
 const Header_Navigation = () => {
+    const [showWelcomeMessage, setShowWelcomeMessage] = useState(true);
+    const [profile, setProfile] = useState({});
+    const [isLoading, setIsLoading] = useState(false);
+    const [isSubmenuVisible, setIsSubmenuVisible] = useState(false);
+
+    const toggleSubmenu = () => {
+        setIsSubmenuVisible(!isSubmenuVisible);
+    };
+  
+    useEffect(() => {
+        const fetchProfile = async () => {
+          setIsLoading(true);
+          try {
+            const token = localStorage.getItem('token');
+            const response = await axios.get('http://localhost:8002/profile', {
+              headers: {
+                  'Authorization': `Bearer ${token}`          },
+            });
+            setProfile(response.data);
+          } catch (error) {
+            console.error('There was a problem with your fetch operation:', error);
+          } finally {
+            setIsLoading(false);
+          }
+        };
+    
+        fetchProfile();
+      }, []);
+      
   return (
     <div className='flex md:h-[80px] bg-white shadow-md items-center justify-between px-[24px] flex-row sm:flex-row h-[60px]'>
       <h4 className='font-inter text-black text-[20px] font-semibold'>Dashboard</h4>
@@ -15,15 +46,25 @@ const Header_Navigation = () => {
         />
         <div className='flex flex-row justify-between w-[72px] items-center'>
             <img 
-                src={profilepic} 
+                src={profile.profilePicture ? `http://localhost:8002/${profile.profilePicture.replace("\\", "/")}` : profilepic}
                 alt='profile'
                 className='h-[40px] w-[40px] ' 
             />
-            <img 
-                src={menuicon} 
-                alt='menu icon'
-                className='h-[24px] w-[24px] cursor-pointer ' 
-            />
+             <div className='relative'>
+    <img 
+        src={menuicon} 
+        alt='menu icon'
+        className='h-[24px] w-[24px] cursor-pointer' 
+        onClick={toggleSubmenu}
+    />
+    {isSubmenuVisible && (
+        <div className="absolute w-auto px-4 mt-2 bg-orange-100 rounded-md shadow-md right-4 submenu">
+            {/* Submenu content here */}
+            <p className="p-2 hover:text-orange-800">Profile</p>
+            <p className="p-2 hover:text-orange-800">Logout</p>
+        </div>
+    )}
+</div>
         </div>
       </div>
     </div>
